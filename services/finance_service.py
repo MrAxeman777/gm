31c92714cea251e644d26216caa4f55de7addfc5
MrAxeman777 <arsharma892@gmail.com>
2026-07-21 14:01:25 -0500

    Update finance_service.py

--- services/finance_service.py
+++ services/finance_service.py
@@ -1,1 +1,12 @@
+class FinanceService:
+    def __init__(self):
+        pass
 
+    def add_money(self, club, amount):
+        club.budget += amount
+
+    def remove_money(self, club, amount):
+        if club.budget >= amount:
+            club.budget -= amount
+            return True
+        return False
