ceb5b022281fe80acb14c17b0a20679b2492d35b
MrAxeman777 <arsharma892@gmail.com>
2026-07-21 14:03:05 -0500

    Update season_service.py

--- services/season_service.py
+++ services/season_service.py
@@ -1,1 +1,17 @@
+class SeasonService:
+    def __init__(self):
+        self.current_season = "2026/27"
+        self.current_week = 1
 
+    def next_week(self):
+        self.current_week += 1
+
+        if self.current_week > 38:
+            self.current_week = 1
+            self.advance_season()
+
+    def advance_season(self):
+        start_year = int(self.current_season[:4])
+        end_year = start_year + 2
+
+        self.current_season = f"{start_year + 1}/{str(end_year)[2:]}"
