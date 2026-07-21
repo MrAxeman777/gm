2911f47af3f4c7953b5b316199742347c38a2bdb
MrAxeman777 <arsharma892@gmail.com>
2026-07-21 14:12:36 -0500

    Update transfer_service.py

--- services/transfer_service.py
+++ services/transfer_service.py
@@ -1,19 +1,21 @@
 class TransferService:
+
     def __init__(self):
         pass
 
-    def buy_player(self, buying_club, selling_club, player):
+
+    def buy_player(self, buying_club, player):
+
+        if player in buying_club.players:
+            return False, "Player already in squad"
+
+
         if buying_club.budget < player.value:
-            return False, "Not enough money."
+            return False, "Not enough money"
 
-        # Transfer money
+
         buying_club.budget -= player.value
-        selling_club.budget += player.value
 
-        # Move player
-        if player in selling_club.players:
-            selling_club.players.remove(player)
+        buying_club.add_player(player)
 
-        buying_club.players.append(player)
-
-        return True, f"{player.name} has signed for {buying_club.name}!"
+        return True, f"{player.name} signed for {buying_club.name}!"
