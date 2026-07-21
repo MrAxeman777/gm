55d2d5b034ca499a31d4b7193830bb798ecde835
MrAxeman777 <arsharma892@gmail.com>
2026-07-21 14:05:02 -0500

    Update league.py

--- models/league.py
+++ models/league.py
@@ -1,1 +1,14 @@
+class League:
+    def __init__(self, name):
+        self.name = name
+        self.clubs = []
 
+    def add_club(self, club):
+        self.clubs.append(club)
+
+    def standings(self):
+        return sorted(
+            self.clubs,
+            key=lambda club: club.squad_rating(),
+            reverse=True
+        )
