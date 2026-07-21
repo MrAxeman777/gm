75171e9fd3edd8c1d6289f00457ee94afa8cfe2a
MrAxeman777 <arsharma892@gmail.com>
2026-07-21 14:00:40 -0500

    Update game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -1,1 +1,22 @@
+from services.finance_service import FinanceService
+from services.transfer_service import TransferService
+from services.match_service import MatchService
+from services.season_service import SeasonService
 
+
+class GameEngine:
+    def __init__(self):
+        self.club = None
+        self.current_week = 1
+        self.current_season = "2026/27"
+
+        self.finance = FinanceService()
+        self.transfer = TransferService()
+        self.match = MatchService()
+        self.season = SeasonService()
+
+    def choose_club(self, club):
+        self.club = club
+
+    def next_week(self):
+        self.current_week += 1
